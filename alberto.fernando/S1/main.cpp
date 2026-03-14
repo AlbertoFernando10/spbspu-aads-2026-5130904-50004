0be03e4bbbbd0d39ab6d278930c45346f4dd5742
Fernando Diaz <fernandodiasfd780518@gmail.com>
2026-03-14 13:04:06 -0300

    Reading Input Data

--- alberto.fernando/S1/main.cpp
+++ alberto.fernando/S1/main.cpp
@@ -7,3 +7,21 @@ using namespace alberto;
 int main() {
   List<std::string> names;
   List<List<unsigned long long>> nums;
+std::string nome;
+
+  while (std::cin >> nome) {
+    names.push_back(nome);
+    nums.push_back(List<unsigned long long>());
+
+    unsigned long long valor;
+
+    while (std::cin >> valor) {
+      nums.back().push_back(valor);
+
+      if (std::cin.peek() == '\n') {
+        break;
+      }
+    }
+
+    std::cin.clear();
+  }
