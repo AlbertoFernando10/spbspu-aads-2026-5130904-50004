03a57a8799ae2a1cf15e7fa22572297ec1cea36e
Fernando Diaz <fernandodiasfd780518@gmail.com>
2026-03-14 11:49:15 -0300

    classes Iterator

--- alberto.fernando/S1/List.h
+++ alberto.fernando/S1/List.h
@@ -8,3 +8,72 @@
 
 namespace alberto {
   template <class T> class List;
+  template <class T>
+  class Iter {
+    friend class List<T>;
+
+  public:
+    Iter() noexcept : ptr(nullptr) {}
+    Iter(const Iter&) noexcept = default;
+    T& operator*() const noexcept { return ptr->data; }
+    T* operator->() const noexcept { return &(ptr->data); }
+    Iter& operator++() noexcept {
+      ptr = ptr->next;
+      return *this;
+    }
+    Iter& operator--() noexcept {
+      ptr = ptr->prev;
+      return *this;
+    }
+    Iter operator++(int) noexcept {
+      Iter tmp = *this;
+      ptr = ptr->next;
+      return tmp;
+    }
+    Iter operator--(int) noexcept {
+      Iter tmp = *this;
+      ptr = ptr->prev;
+      return tmp;
+    }
+    bool operator==(const Iter& other) const noexcept { return ptr == other.ptr; }
+    bool operator!=(const Iter& other) const noexcept { return ptr != other.ptr; }
+
+  private:
+    typename List<T>::Elem* ptr;
+    explicit Iter(typename List<T>::Elem* p) noexcept : ptr(p) {}
+  };
+
+  template <class T>
+  class CIter {
+    friend class List<T>;
+
+  public:
+    CIter() noexcept : ptr(nullptr) {}
+    CIter(const CIter&) noexcept = default;
+    const T& operator*() const noexcept { return ptr->data; }
+    const T* operator->() const noexcept { return &(ptr->data); }
+    CIter& operator++() noexcept {
+      ptr = ptr->next
+      return *this;
+    }
+    CIter& operator--() noexcept {
+      ptr = ptr->prev;
+      return *this;
+    }
+    CIter operator++(int) noexcept {
+      CIter tmp = *this;
+      ptr = ptr->next;
+      return tmp;
+    }
+    CIter operator--(int) noexcept {
+      CIter tmp = *this;
+      ptr = ptr->prev;
+      return tmp;
+    }
+    bool operator==(const CIter& other) const noexcept { return ptr == other.ptr; }
+    bool operator!=(const CIter& other) const noexcept { return ptr != other.ptr; }
+
+  private:
+    const typename List<T>::Elem* ptr;
+    explicit CIter(const typename List<T>::Elem* p) noexcept : ptr(p) {}
+  };
