df37c30a0c9873775d0690929940dbc465bcf59a
Fernando Diaz <fernandodiasfd780518@gmail.com>
2026-03-14 13:19:06 -0300

    Processing and Output of Results

--- alberto.fernando/S1/main.cpp
+++ alberto.fernando/S1/main.cpp
@@ -48,3 +48,39 @@ size_t max_size = 0;
     std::cout << "0\n";
     return 0;
   }
+List<unsigned long long> sums;
+  try {
+    for (size_t col = 0; col < max_size; ++col) {
+      unsigned long long current_sum = 0;
+      bool first_in_row = true;
+      auto nums_it = nums.cbegin();
+      for (size_t i = 0; i < nums.size(); ++i) {
+        if (col < nums_it->size()) {
+          unsigned long long val = (*nums_it)[col];
+          if (!first_in_row) std::cout << " ";
+          std::cout << val;
+          alberto::sum(current_sum, val);
+          first_in_row = false;
+        }
+        ++nums_it;
+      }
+      if (!first_in_row) {
+        std::cout << "\n";
+        sums.push_back(current_sum);
+      }
+    }
+    first = true;
+    for (auto it = sums.cbegin(); it != sums.cend(); ++it) {
+      if (!first) std::cout << " ";
+      std::cout << *it;
+      first = false;
+    }
+    std::cout << "\n";
+  } catch (const std::overflow_error&) {
+    std::cout << "\n";
+    std::cerr << "Overflow\n";
+    return 1;
+  }
+
+  return 0;
+}
