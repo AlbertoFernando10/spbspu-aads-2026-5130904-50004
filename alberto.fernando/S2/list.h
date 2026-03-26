23303d8a0602b88f07da0885749a8c0d91c5bbfe
Fernando Diaz <fernandodiasfd780518@gmail.com>
2026-03-26 01:12:44 -0300

    private attributes, constructors, destructor, and swap

--- alberto.fernando/S2/list.h
+++ alberto.fernando/S2/list.h
@@ -13,3 +13,29 @@ class List {
         Node* next;
         explicit Node(T v, Node* n = nullptr) : value(std::move(v)), next(n) {}
     };
+    Node*  head_  = nullptr;
+    Node*  tail_  = nullptr;
+    size_t size_  = 0;
+
+public:
+    List() = default;
+    List(const List& o) {
+        for (Node* n = o.head_; n; n = n->next)
+            push_back(n->value);
+    }
+    List(List&& o) noexcept
+        : head_(o.head_), tail_(o.tail_), size_(o.size_) {
+        o.head_ = o.tail_ = nullptr;
+        o.size_ = 0;
+    }
+    List& operator=(List o) noexcept {
+        swap(o);
+        return *this;
+    }
+    ~List() { clear(); }
+
+    void swap(List& o) noexcept {
+        std::swap(head_, o.head_);
+        std::swap(tail_, o.tail_);
+        std::swap(size_, o.size_);
+    }
