724d1b25052929cf59f761887f6841c89d0750a3
Fernando Diaz <fernandodiasfd780518@gmail.com>
2026-05-22 18:38:43 -0300

    add Constructors and Destructor

--- alberto.fernando/S4/bstnode.hpp
+++ alberto.fernando/S4/bstnode.hpp
@@ -15,3 +15,34 @@ struct BSTNode {
   BSTNode *left_;
   BSTNode *right_;
   pair_t  *data_;
+  explicit BSTNode(Kind k) noexcept:
+    kind_(k),
+    parent_(nullptr),
+    left_(nullptr),
+    right_(nullptr),
+    data_(nullptr)
+  {}
+
+  BSTNode(const Key &k, const Value &v, BSTNode *fakeLeaf):
+    kind_(Kind::REAL),
+    parent_(nullptr),
+    left_(fakeLeaf),
+    right_(fakeLeaf),
+    data_(new pair_t(k, v))
+  {}
+
+  BSTNode(Key &&k, Value &&v, BSTNode *fakeLeaf):
+    kind_(Kind::REAL),
+    parent_(nullptr),
+    left_(fakeLeaf),
+    right_(fakeLeaf),
+    data_(new pair_t(std::move(k), std::move(v)))
+  {}
+
+  ~BSTNode()
+  {
+    delete data_;
+  }
+
+  BSTNode(const BSTNode &) = delete;
+  BSTNode &operator=(const BSTNode &) = delete;
