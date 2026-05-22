d51cca074f8374ddf31f1bdaf88c58165e947ae9
Fernando Diaz <fernandodiasfd780518@gmail.com>
2026-05-22 19:22:49 -0300

    add main file

--- alberto.fernando/S4/main.cpp
+++ alberto.fernando/S4/main.cpp
@@ -1,5 +1,24 @@
-#include <iostream>
-int main()
-{
+#include "commands.hpp"
 
+#include <iostream>
+#include <stdexcept>
+
+int main(int argc, char *argv[])
+{
+  if (argc < 2) {
+    std::cerr << "Usage: " << argv[0] << " <data-file>\n";
+    return 1;
+  }
+
+  alberto::OuterTree datasets;
+
+  try {
+    alberto::loadFile(argv[1], datasets);
+  } catch (const std::exception &e) {
+    std::cerr << e.what() << '\n';
+    return 1;
+  }
+
+  alberto::repl(datasets);
+  return 0;
 }
