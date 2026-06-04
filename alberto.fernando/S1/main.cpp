3c892c5d88c7d381b91ded3273ab365a0c5d10d0
Fernando Diaz <fernandodiasfd780518@gmail.com>
2026-06-04 19:07:46 -0300

    another correction main.cpp

--- alberto.fernando/S1/main.cpp
+++ alberto.fernando/S1/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include "List.h"
+
 using namespace alberto;
 int main() {
   List<std::string> names;
@@ -22,13 +23,6 @@ int main() {
     std::cout << "0\n";
     return 0;
   }
-  bool first = true;
-  for (auto it = names.cbegin(); it != names.cend(); ++it) {
-    if (!first) std::cout << " ";
-    std::cout << *it;
-    first = false;
-  }
-  std::cout << "\n";
   size_t max_size = 0;
   for (auto it = nums.cbegin(); it != nums.cend(); ++it) {
     if (it->size() > max_size) {
@@ -43,40 +37,45 @@ int main() {
   try {
     for (size_t col = 0; col < max_size; ++col) {
       unsigned long long current_sum = 0;
-      bool has_values = false;
       auto nums_it = nums.cbegin();
       for (size_t i = 0; i < nums.size(); ++i) {
         if (col < nums_it->size()) {
           alberto::sum(current_sum, (*nums_it)[col]);
-          has_values = true;
         }
         ++nums_it;
       }
-      if (has_values) {
-        bool first_in_col = true;
-        nums_it = nums.cbegin();
-        for (size_t i = 0; i < nums.size(); ++i) {
-          if (col < nums_it->size()) {
-            if (!first_in_col) std::cout << " ";
-            std::cout << (*nums_it)[col];
-            first_in_col = false;
-          }
-          ++nums_it;
-        }
-        std::cout << "\n";
-        sums.push_back(current_sum);
-      }
+      sums.push_back(current_sum);
     }
-    first = true;
-    for (auto it = sums.cbegin(); it != sums.cend(); ++it) {
-      if (!first) std::cout << " ";
-      std::cout << *it;
-      first = false;
-    }
-    std::cout << "\n";
   } catch (const std::overflow_error&) {
     std::cerr << "Overflow\n";
     return 1;
   }
+  bool first = true;
+  for (auto it = names.cbegin(); it != names.cend(); ++it) {
+    if (!first) std::cout << " ";
+    std::cout << *it;
+    first = false;
+  }
+  std::cout << "\n";
+  for (size_t col = 0; col < max_size; ++col) {
+    bool first_in_col = true;
+    auto nums_it = nums.cbegin();
+    for (size_t i = 0; i < nums.size(); ++i) {
+      if (col < nums_it->size()) {
+        if (!first_in_col) std::cout << " ";
+        std::cout << (*nums_it)[col];
+        first_in_col = false;
+      }
+      ++nums_it;
+    }
+    if (!first_in_col) std::cout << "\n";
+  }
+  first = true;
+  for (auto it = sums.cbegin(); it != sums.cend(); ++it) {
+    if (!first) std::cout << " ";
+    std::cout << *it;
+    first = false;
+  }
+  std::cout << "\n";
   return 0;
 }
