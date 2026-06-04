3d2452b099e59cdf5443d4cf4d507544bfa7b821
Fernando Diaz <fernandodiasfd780518@gmail.com>
2026-06-04 16:59:09 -0300

    add CIter class

--- alberto.fernando/S1/Iter.h
+++ alberto.fernando/S1/Iter.h
@@ -67,3 +67,71 @@ namespace alberto {
       ptr(p)
     {}
   };
+  template < class T >
+  class CIter: public std::iterator< std::bidirectional_iterator_tag, T,
+      std::ptrdiff_t, const T*, const T& > {
+    friend class List< T >;
+
+  public:
+    CIter() noexcept :
+      ptr(nullptr)
+    {}
+
+    CIter(const CIter&) noexcept = default;
+
+    const T& operator*() const noexcept
+    {
+      return ptr->data_;
+    }
+
+    const T* operator->() const noexcept
+    {
+      return &(ptr->data_);
+    }
+
+    CIter& operator++() noexcept
+    {
+      ptr = ptr->next_;
+      return *this;
+    }
+
+    CIter& operator--() noexcept
+    {
+      ptr = ptr->prev_;
+      return *this;
+    }
+
+    CIter operator++(int) noexcept
+    {
+      CIter tmp = *this;
+      ptr = ptr->next_;
+      return tmp;
+    }
+
+    CIter operator--(int) noexcept
+    {
+      CIter tmp = *this;
+      ptr = ptr->prev_;
+      return tmp;
+    }
+
+    bool operator==(const CIter& other) const noexcept
+    {
+      return ptr == other.ptr;
+    }
+
+    bool operator!=(const CIter& other) const noexcept
+    {
+      return ptr != other.ptr;
+    }
+
+  private:
+    const typename List< T >::Elem* ptr;
+
+    explicit CIter(const typename List< T >::Elem* p) noexcept :
+      ptr(p)
+    {}
+  };
+}
+
+#endif
