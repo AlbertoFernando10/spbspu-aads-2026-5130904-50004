6d96e42947853b7a78b69b568c7df5bf785ce9bc
Fernando Diaz <fernandodiasfd780518@gmail.com>
2026-06-04 16:38:30 -0300

    constructors and dereference operators of Iter

--- alberto.fernando/S1/Iter.h
+++ alberto.fernando/S1/Iter.h
@@ -9,3 +9,19 @@ namespace alberto {
   template < class T >
   class Iter: public std::iterator< std::bidirectional_iterator_tag, T > {
     friend class List< T >;
+  public:
+    Iter() noexcept :
+      ptr(nullptr)
+    {}
+
+    Iter(const Iter&) noexcept = default;
+
+    T& operator*() const noexcept
+    {
+      return ptr->data_;
+    }
+
+    T* operator->() const noexcept
+    {
+      return &(ptr->data_);
+    }
