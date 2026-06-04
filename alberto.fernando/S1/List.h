00144e9f7d35181dd3ece22280963484d3e6b24c
Fernando Diaz <fernandodiasfd780518@gmail.com>
2026-06-04 15:58:33 -0300

    add constructors, destructor, assignment operators, swap, empty, size, clear

--- alberto.fernando/S1/List.h
+++ alberto.fernando/S1/List.h
@@ -33,3 +33,74 @@ namespace alberto {
     Elem* head_;
     Elem* tail_;
     size_t sz_;
+  public:
+    List() noexcept :
+      head_(nullptr),
+      tail_(nullptr),
+      sz_(0)
+    {}
+
+    List(const List& other) :
+      head_(nullptr),
+      tail_(nullptr),
+      sz_(0)
+    {
+      for (Elem* curr = other.head_; curr != nullptr; curr = curr->next_) {
+        push_back(curr->data_);
+      }
+    }
+
+    List(List&& other) noexcept :
+      head_(other.head_),
+      tail_(other.tail_),
+      sz_(other.sz_)
+    {
+      other.head_ = nullptr;
+      other.tail_ = nullptr;
+      other.sz_ = 0;
+    }
+
+    ~List()
+    {
+      clear();
+    }
+
+    List& operator=(const List& other)
+    {
+      if (this != &other) {
+        List tmp(other);
+        swap(tmp);
+      }
+      return *this;
+    }
+
+    List& operator=(List&& other) noexcept
+    {
+      List tmp(std::move(other));
+      swap(tmp);
+      return *this;
+    }
+
+    void swap(List& other) noexcept
+    {
+      std::swap(head_, other.head_);
+      std::swap(tail_, other.tail_);
+      std::swap(sz_, other.sz_);
+    }
+
+    bool empty() const noexcept
+    {
+      return sz_ == 0;
+    }
+
+    size_t size() const noexcept
+    {
+      return sz_;
+    }
+
+    void clear()
+    {
+      while (!empty()) {
+        pop_front();
+      }
+    }
