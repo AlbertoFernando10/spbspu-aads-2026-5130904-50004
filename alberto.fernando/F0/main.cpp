353ea050381ba6ffac6d7bcff790425ffa8f0828
Fernando Diaz <fernandodiasfd780518@gmail.com>
2026-06-12 10:06:31 -0300

    add input line reading loop

--- alberto.fernando/F0/main.cpp
+++ alberto.fernando/F0/main.cpp
@@ -7,3 +7,12 @@ int main()
   alberto::Session session;
   alberto::HashTable< std::string, alberto::CmdFn, alberto::xx_hash > cmds(32);
   alberto::buildCommands(cmds);
+  std::string line;
+  while (std::getline(std::cin, line)) {
+    if (line.empty()) {
+      continue;
+    }
+    const auto tok = alberto::tokenize(line);
+    if (tok.empty()) {
+      continue;
+    }
