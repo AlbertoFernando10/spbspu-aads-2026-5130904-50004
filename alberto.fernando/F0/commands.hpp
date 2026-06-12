0bb4cc196dfacd867297844ffa1666bd0452df68
Fernando Diaz <fernandodiasfd780518@gmail.com>
2026-06-12 12:48:24 -0300

    Command function declarations

--- alberto.fernando/F0/commands.hpp
+++ alberto.fernando/F0/commands.hpp
@@ -27,3 +27,11 @@ struct Session {
 using CmdFn = std::function< void(Session&, const std::vector< std::string >&) >;
 
 std::vector< std::string > tokenize(const std::string& line);
+void cmdLocal(Session& s, const std::vector< std::string >& tok);
+void cmdShowText(Session& s, const std::vector< std::string >& tok);
+void cmdDropText(Session& s, const std::vector< std::string >& tok);
+void cmdListTexts(Session& s, const std::vector< std::string >& tok);
+void cmdBuildCoding(Session& s, const std::vector< std::string >& tok);
+void cmdShowCodes(Session& s, const std::vector< std::string >& tok);
+void cmdDropCoding(Session& s, const std::vector< std::string >& tok);
+void cmdListCodings(Session& s, const std::vector< std::string >& tok);
