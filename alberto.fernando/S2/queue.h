4db82f0ee56503d7f5fbec8253fef7d699a7afde
Fernando Diaz <fernandodiasfd780518@gmail.com>
2026-06-25 14:52:23 -0300

    fix queue.h mistakes

--- alberto.fernando/S2/queue.h
+++ alberto.fernando/S2/queue.h
@@ -8,42 +8,60 @@
 
 namespace alberto {
 
-template<typename T>
+template< typename T >
 class Queue {
-    List<T> data_;
+  List< T > data_;
 
 public:
-    Queue()                       = default;
-    Queue(const Queue&)           = default;
-    Queue(Queue&&)                = default;
-    Queue& operator=(const Queue&) = default;
-    Queue& operator=(Queue&&)     = default;
+  Queue() = default;
+  Queue(const Queue&) = default;
+  Queue(Queue&&) = default;
+  Queue& operator=(const Queue&) = default;
+  Queue& operator=(Queue&&) = default;
 
-    bool   empty() const noexcept { return data_.empty(); }
-    size_t size()  const noexcept { return data_.size(); }
+  bool empty() const noexcept
+  {
+    return data_.empty();
+  }
 
-    void push(const T& value) {
-        data_.push_back(value);
+  size_t size() const noexcept
+  {
+    return data_.size();
+  }
+
+  void push(const T& value)
+  {
+    data_.push_back(value);
+  }
+
+  void push(T&& value)
+  {
+    data_.push_back(std::move(value));
+  }
+
+  void pop()
+  {
+    if (empty()) {
+      throw std::out_of_range("Queue::pop: queue is empty");
     }
+    data_.pop_front();
+  }
 
-    void push(T&& value) {
-        data_.push_back(std::move(value));
+  const T& front() const
+  {
+    if (empty()) {
+      throw std::out_of_range("Queue::front: queue is empty");
     }
+    return data_.front();
+  }
 
-    T drop() {
-        if (empty()) throw std::out_of_range("Queue::drop: queue is empty");
-        return data_.pop_front();
+  T& front()
+  {
+    if (empty()) {
+      throw std::out_of_range("Queue::front: queue is empty");
     }
-
-    const T& front() const {
-        if (empty()) throw std::out_of_range("Queue::front: queue is empty");
-        return data_.front();
-    }
-
-    T& front() {
-        if (empty()) throw std::out_of_range("Queue::front: queue is empty");
-        return data_.front();
-    }
+    return data_.front();
+  }
 };
 
 }
